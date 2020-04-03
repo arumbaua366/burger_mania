9bd7e55cd190d534b823e2397bbc891c45c4280b
ar <56378858+arumbaua366@users.noreply.github.com>
2020-04-02 22:58:10 -0700

    added all folders and set up files

---
+++ server.js
@@ -0,0 +1,29 @@
+  
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+const routes = require("./controllers/burgercontroller.js");
+
+app.use(routes);
+
+
+app.listen(PORT, function() {
+ 
+  console.log("Server listening on: http://localhost:" + PORT);
+});
